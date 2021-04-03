92dfd9198a5546066825b88f37905e6b35c693df
EricBZTF <eric.fersini@gmail.com>
2021-04-03 21:10:50 +0200

    perfected drag&drop

--- Assets/dragAndBuild.cs
+++ Assets/dragAndBuild.cs
@@ -2,43 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dragDrop : MonoBehaviour
+public class dragAndBuild : MonoBehaviour
 {
+
     private bool isDragging;
     private bool build = false;
     private bool isOnFree = false;
     Renderer rend;
+
+    //Arrays for all the freeSpaces
     public GameObject[] fSpaces;
     public GameObject freeObject;
+
+    //Used to destroy 
     private bool yeetThatBitch = false;
 
-    
-
-    //get position of free Space
+    //Variables for Free Spaces
     private Vector2 freeSpace;
     private Vector2 thisFreeSpace;
 
-    //get edges of free Space
+    //Variabels for Edges of Free Spaces
     private Vector2 freeSpaceCopyTop;
     private Vector2 freeSpaceCopyLeft;
     private Vector2 freeSpaceCopyRight;
 
-    //Get original location
-    private Vector3 originalLoc;
+    // Used for Copyying
     public GameObject sPrefab;
     public GameObject sPrefabeClone;
 
 
-
+    // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         fSpaces = GameObject.FindGameObjectsWithTag("FreeSpace");
-        originalLoc = transform.position;
         sPrefab = GameObject.Find("SquarePrefab");
-        
     }
 
+    // Sets draggin to true and makes a copy of gameObject
     void OnMouseDown()
     {
         isDragging = true;
@@ -50,17 +51,21 @@ public class dragDrop : MonoBehaviour
         }
     }
 
+    // Sets draggin to false and yeetThatBitch to true
     void OnMouseUp()
     {
         isDragging = false;
         yeetThatBitch = true;
     }
 
+    // Update is called once per frame
+    // Checks if Object is on a freeSpace or not
     void Update()
     {
         for (int i = 0; i < fSpaces.Length; i++)
         {
             freeSpace = fSpaces[i].transform.position;
+            freeObject = fSpaces[i];
 
             //left edge
             freeSpaceCopyLeft = freeSpace;
@@ -75,9 +80,9 @@ public class dragDrop : MonoBehaviour
             //Top Edge
             freeSpaceCopyTop = freeSpace;
             freeSpaceCopyTop.y += 10;
-            
 
-            if (build == false)
+
+            if (build == false && fSpaces[i].tag != "usedSpace")
             {
                 //Check if mouse is pressed and move the object accordingly
                 if (isDragging)
@@ -89,18 +94,16 @@ public class dragDrop : MonoBehaviour
                 //Check if Object is in area to be build upon
                 if (Vector2.Distance(freeSpace, this.transform.position) < 5.5 && Vector2.Distance(freeSpaceCopyTop, this.transform.position) < 5.5)
                 {
-                    Debug.Log(freeSpaceCopyTop);
-                    Debug.Log(freeSpace);
                     rend.material.color = Color.green;
                     thisFreeSpace = fSpaces[i].transform.position;
                     isOnFree = true;
-                    
+                    Debug.Log(freeObject);
                 }
 
                 //check if Objekt is not on a freespace when mouse stops being clicked
                 else if (isDragging == false && Vector2.Distance(freeSpace, transform.position) > 1 && isOnFree == false)
                 {
-                    //Yeet it back to its starting position
+                    //Destroys Object if it is not on a free Space
                     if (yeetThatBitch)
                     {
                         Destroy(gameObject);
@@ -110,25 +113,29 @@ public class dragDrop : MonoBehaviour
                 //Check if Object is not on a free Space and change color to white
                 else if (isDragging == true && Vector2.Distance(thisFreeSpace, transform.position) < 4 || Vector2.Distance(freeSpaceCopyTop, this.transform.position) < 4 || Vector2.Distance(freeSpaceCopyLeft, this.transform.position) < 4 || Vector2.Distance(freeSpaceCopyRight, this.transform.position) < 4)
                 {
-                   rend.material.color = Color.white;
-                   isOnFree = false;
+                    rend.material.color = Color.white;
+                    isOnFree = false;
                 }
 
                 //check if Object is build on a free space
-                if (isDragging == false && Vector2.Distance(freeSpace, this.transform.position) < 5.5 && Vector2.Distance(freeSpaceCopyTop, this.transform.position) < 5.5)
+                if (isDragging == false && Vector2.Distance(freeSpace, this.transform.position) < 6 && Vector2.Distance(freeSpaceCopyTop, this.transform.position) < 6)
                 {
                     build = true;
+                    freeObject.tag = "usedSpace";
+                    
                 }
 
             }
 
         }
-
+        //changes name if it is build
         if (build)
         {
-            transform.gameObject.tag = "buildWood";
             transform.gameObject.name = "buildWood";
+            gameObject.layer = freeObject.layer;
+
+            rend = GetComponent<SpriteRenderer>();
+            rend.sortingLayerName = "Background4";
         }
     }
 }
-
